0941896be4beaa9c01bc9ef6558a040b2feb4f23
karmerk <karmerk@gmail.com>
2021-11-11 20:58:29 +0100

    Crud concept + experimental improvements

---
+++ src/WebApi/Crud/WebApplicationExtensions.cs
@@ -0,0 +1,75 @@
+﻿namespace WebApi.Crud
+{
+    public static class WebApplicationExtensions
+    {
+        public static WebApplication AddCrud<T>(this WebApplication webApplication, string pattern, Action<ICrudBuilder<T>> configure)
+        {
+            var builder = new CrudBuilder<T>(webApplication, pattern);
+
+            configure(builder);
+
+            return webApplication;
+        }
+    }
+
+    public interface ICrudBuilder<T>
+    {
+        public ICrudBuilder<T> AddCreate<THandler>() where THandler : IHandler<T, T>;
+        public ICrudBuilder<T> AddRead<THandler, TKey>(Func<T, TKey>? selector = null) where THandler : IHandler<TKey, T>;
+
+        //public ICrudBuilder<T> AddUpdate<THandler, TKey>() where THandler : IHandler<(TKey, T), T>;
+        //public ICrudBuilder<T> AddDelete<THandler, TKey>() where THandler : IHandler<TKey, object?>;
+        //public ICrudBuilder<T> AddList<THandler>() where THandler : IHandler<object?, IEnumerable<T>>;
+    }
+
+    internal class CrudBuilder<T> : ICrudBuilder<T>
+    {
+        private readonly WebApplication _webApplication;
+        private readonly string _pattern;
+
+        public CrudBuilder(WebApplication webApplication, string pattern)
+        {
+            _webApplication = webApplication;
+            _pattern = pattern;
+        }
+
+        public ICrudBuilder<T> AddCreate<THandler>() where THandler : IHandler<T, T>
+        {
+            _webApplication.UseEndpoints(configure => configure.MapMethods(_pattern, new[] { "POST" }, EndpointHandler.Delegate<THandler, T, T>()));
+
+            return this;
+        }
+
+        public ICrudBuilder<T> AddRead<THandler, TKey>(Func<T, TKey>? selector = null) where THandler : IHandler<TKey, T>
+        {
+            var options = new HandlingOptions()
+            {
+                DeserializeRequestFrom = HandlingOptions.RequestOrigin.RouteValues
+            };
+
+            _webApplication.UseEndpoints(configure => configure.MapMethods($"{_pattern}/{{key}}", new[] { "GET" }, EndpointHandler.Delegate<THandler, TKey, T>(options)));
+
+            return this;
+        }
+    }   
+}
+
+public interface ICreateHandler<T> : IHandler<T, T>
+{
+
+}
+
+public interface IReadHandler<T> : IHandler<Uri, T>
+{
+
+}
+
+public interface IUpdateHandler<T> : IHandler<(Uri, T), T>
+{
+
+}
+
+public interface IDeleteHandler<T> : IHandler<Uri, object?>
+{
+
+}
--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,9 +1,9 @@
+using WebApi.Crud;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// TODO should also have an option to AddAllRequestHandlers(Assembly);
-
-builder.Services.AddScoped<MyHandler>();
-builder.Services.AddScoped<ReadRessourceHandler>();
+builder.Services.AddSingleton<Items>(_ => new Items(3));
+builder.Services.AddEndpointHandlers();
 builder.Services.AddHttpContextAccessor();
 
 var app = builder.Build();
@@ -14,8 +14,16 @@ app.MapGet("/", () => "Hello World!");
 
 // lets drop the concept with RequestHandlers and make it EndpointHandlers
 
-app.UseEndpoints(app => app.MapGet("/items", EndpointHandler.Delegate<ReadRessourceHandler, int, MyResponse>()));
+app.UseEndpoints(app => app.MapGet("/test", EndpointHandler.Delegate<ReadRessourceHandler, int, MyResponse>()));
 app.UseEndpoints(app => app.MapPost("/bam", EndpointHandler.Delegate<MyHandler, MyRequest, MyResponse>()));
+app.UseEndpoints(app => app.MapGet("/items", EndpointHandler.Delegate<ListItemsHandler, object?, IEnumerable<Item>>()));
+
+// Add Crud
+app.AddCrud<Item>("/item", x =>
+{
+    x.AddCreate<ItemCreateHandler>();
+    x.AddRead<ItemReadHandler, int>();
+});
 
 app.Run();
 
@@ -49,16 +57,32 @@ public sealed class EndpointHandler
             var logger = context.RequestServices.GetRequiredService<ILogger<THandler>>();
 
             var httpRequest = context.Request;
+            TRequest request = default!;
 
-            if(!httpRequest.HasJsonContentType())
+            // Also request should be able to come from body, querystring, routevalues and form? that'll be fun to implement
+            switch (options?.DeserializeRequestFrom)
             {
-                throw new InvalidOperationException("Only JSON content type supported right now");
+                case HandlingOptions.RequestOrigin.QueryString:
+                    break;
+                case HandlingOptions.RequestOrigin.RouteValues:
+                    (_, request) = await new RouteValueRequestReader().TryReadAsync<TRequest>(httpRequest);
+                    break;
+                case HandlingOptions.RequestOrigin.Form:
+                    break;
+                case HandlingOptions.RequestOrigin.TryThemAll:
+                    break;
+                default:
+                    (_, request) = await new JsonRequestReader().TryReadAsync<TRequest>(httpRequest);
+                    break;
             }
 
-            // If TRequest nullable then we should accept no input ! 
-            // Also request should be able to come from body, querystring, routevalues and form? that'll be fun to implement
-            var request = await httpRequest.ReadFromJsonAsync<TRequest>() ?? throw new ArgumentException($"Could not read {typeof(TRequest).Name} as JSON from request body");
+            if( request == null && Nullable.GetUnderlyingType(typeof(TRequest)) != null)
+            {
+                throw new ArgumentException($"Failed to read {typeof(TRequest).Name} from HTTP request");
+            }
+                       
             
+                        
             var handler = context.RequestServices.GetRequiredService<THandler>();
             var response = await handler.HandleAsync(request, CancellationToken.None);
 
@@ -68,10 +92,79 @@ public sealed class EndpointHandler
             }
         });
     }
-    
+
 
     // Can we make a method that does not need to specify the TRequest and TResponse explicit ?
+
+    // app.UseEndpoints(app => app.MapGet("/items", EndpointHandler.Delegate<ListItemsHandler>())); 
 }
+
+public class JsonRequestReader
+{
+    // Do a better API and pack it away
+    public async Task<(bool, T)> TryReadAsync<T>(HttpRequest request)
+    {
+        try
+        {
+            var value = await request.ReadFromJsonAsync<T>();
+
+            return (value != null, value!);
+        }
+        catch
+        {
+            // buhuu do this smarter
+        }
+
+        return (false, default!);
+    }
+}
+
+public class RouteValueRequestReader
+{
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+    public async Task<(bool, T)> TryReadAsync<T>(HttpRequest request)
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+    {
+        var value = request.RouteValues.FirstOrDefault().Value;
+
+        if (value != default)
+        {
+            if (typeof(T) == typeof(int) && value is string str)
+            {
+                object integer = Convert.ToInt32(str);
+
+                return (true, (T)integer);
+            }
+             
+            if(typeof(T) == typeof(string) && value is string)
+            {
+                return (true, (T)value);
+            }
+        }
+        return (false, default!);
+    }
+}
+
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddEndpointHandlers(this IServiceCollection serviceCollection)
+    {
+        var type = typeof(IHandler<,>);
+        var handlerTypes = AppDomain.CurrentDomain.GetAssemblies()
+            .SelectMany(x => x.DefinedTypes)
+            .Where(x=>x.IsClass && !x.IsAbstract)
+            .Where(x => x.ImplementedInterfaces.Any(y => y.IsGenericType && y.GetGenericTypeDefinition() == type));
+
+        foreach (var handlerType in handlerTypes)
+        {
+            serviceCollection.AddScoped(handlerType);
+        }
+
+        return serviceCollection;
+    }
+}
+
 
 public sealed class HandlingOptions
 {
@@ -140,3 +233,79 @@ public class ReadRessourceHandler : IHandler<int, MyResponse>
         return Task.FromResult(response);
     }
 }
+
+
+public sealed class ListItemsHandler : IHandler<object?, IEnumerable<Item>>
+{
+    private readonly Items _items;
+
+    public ListItemsHandler(Items items)
+    {
+        _items = items;
+    }
+
+    public async Task<IEnumerable<Item>> HandleAsync(object? request, CancellationToken cancellationToken)
+    {
+        await Task.CompletedTask; // Awesome
+
+        return _items.Values.Values; // Values.Values :)
+    }
+}
+
+
+
+
+
+// Playing around with CRUD
+
+public record Item(int Id, string Name)
+{
+}
+
+public sealed class Items
+{
+    public IDictionary<int, Item> Values { get; } = new Dictionary<int,Item>();
+
+    public Items(int count)
+    {
+        foreach(var id in Enumerable.Range(1, count))
+        {
+            Values.Add(id, new Item(id, $"Name + {id}"));
+        }
+    }
+}
+
+public sealed class ItemCreateHandler : IHandler<Item, Item>
+{
+    private readonly Items _items;
+
+    public ItemCreateHandler(Items items)
+    {
+        _items = items;
+    }
+
+    public Task<Item> HandleAsync(Item request, CancellationToken cancellationToken)
+    {
+        _items.Values.Add(request.Id, request);
+
+        return Task.FromResult(request);
+    }
+}
+
+public sealed class ItemReadHandler : IHandler<int, Item>
+{
+    private readonly Items _items;
+
+    public ItemReadHandler(Items items)
+    {
+        _items = items;
+    }
+
+    public Task<Item> HandleAsync(int request, CancellationToken cancellationToken)
+    {
+        var response = _items.Values[request];
+
+        return Task.FromResult(response);
+    }
+}
+
